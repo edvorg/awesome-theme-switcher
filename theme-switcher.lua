659eada5c40f140a477f2abd83f6f69a568e5381
Edward Knyshov <edvorg@gmail.com>
2014-06-30 19:22:22 +0400

    + license

--- theme-switcher.lua
+++ theme-switcher.lua
@@ -1,3 +1,29 @@
+--[[
+
+Awesome Theme Switcher LGPL Source Code
+Copyright (C) 2014 Edward Knyshov.
+
+This file is part of the Awesome Theme Switcher LGPL Source Code
+(Awesome Theme Switcher Source Code).
+
+Awesome Theme Switcher Source Code is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Awesome Theme Switcher Source Code is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Awesome Theme Switcher Source Code.  If not, see <http://www.gnu.org/licenses/>.
+
+Original Awesome Theme Switcher Source Code repository can be found at
+https://github.com/edvorg/awesome-theme-switcher
+
+]]--
+
 local naughty = require("naughty")
 local awful = require("awful")
 local wibox = require("wibox")
